435ccc494f5d558a6bc188cbd2304b0e0297702d
pranita p <pakhalepranita@gmail.com>
2023-05-07 09:14:27 +0530

    added array test

---
+++ src/HelloPranitaTestFile.java
@@ -0,0 +1,9 @@
+
+public class HelloPranitaTestFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/PrintArrayONeD.java
@@ -0,0 +1,17 @@
+
+public class PrintArrayONeD {
+
+	public static void main(String[] args) {
+		int arr[] =  new int[4];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+
+		for (int i=0; i<arr.length ; i++){
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+}
